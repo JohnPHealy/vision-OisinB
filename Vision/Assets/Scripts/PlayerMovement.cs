b97128b0c12acbf6cd858150d304d28e569f4493
oisin <c20387516@mytudublin.ie>
2021-04-18 21:56:18 +0100

    basicBuild

--- Vision/Assets/Scripts/PlayerMovement.cs
+++ Vision/Assets/Scripts/PlayerMovement.cs
@@ -1,42 +1,46 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
-using UnityEngine.Events;
 
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private float moveSpeed, maxSpeed, jumpForce;
     [SerializeField] private Collider2D groundCheck;
     [SerializeField] private LayerMask groundLayers;
-    [SerializeField] private UnityEvent<string> yTracker;
+    [SerializeField] private bool cancelJumpEnabled;
 
     private float moveDir;
-    public Rigidbody2D myRB;
+    private Rigidbody2D myRB;
     private bool canJump;
-    public float posY;
+    private SpriteRenderer mySprite;
 
-    public void Start()
+    private void Start()
     {
         myRB = GetComponent<Rigidbody2D>();
-    }
-
-    private void position()
-    {
-        posY = myRB.position.y + 3;
-        yTracker.Invoke(posY.ToString("0"));
+        mySprite = GetComponentInChildren<SpriteRenderer>();
     }
 
     private void FixedUpdate()
     {
+        if (moveDir > 0)
+        {
+            mySprite.flipX = false;
+        }
+
+        if (moveDir < 0)
+        {
+            mySprite.flipX = true;
+        }
         var moveAxis = Vector3.right * moveDir;
 
-        if (-maxSpeed < myRB.velocity.x && myRB.velocity.x < maxSpeed)
+        if (Mathf.Abs(myRB.velocity.x) < maxSpeed)
         {
             myRB.AddForce(moveAxis * moveSpeed, ForceMode2D.Force);
         }
 
-        if(groundCheck.IsTouchingLayers(groundLayers))
+        if (groundCheck.IsTouchingLayers(groundLayers))
         {
             canJump = true;
         }
@@ -44,6 +48,7 @@ public class PlayerMovement : MonoBehaviour
         {
             canJump = false;
         }
+
     }
 
     public void Move(InputAction.CallbackContext context)
@@ -51,15 +56,23 @@ public class PlayerMovement : MonoBehaviour
         moveDir = context.ReadValue<float>();
     }
 
+    public void Move(float moveAmt)
+    {
+        moveDir = moveAmt;
+    }
+
     public void Jump(InputAction.CallbackContext context)
     {
         if (canJump)
         {
-            myRB.AddForce(Vector3.up * jumpForce, ForceMode2D.Impulse);
-            canJump = false;
+            if (context.started)
+            {
+                myRB.AddForce(Vector3.up * jumpForce, ForceMode2D.Impulse);
+                canJump = false;
+            }
         }
 
-        if(context.canceled)
+        if (context.canceled && cancelJumpEnabled)
         {
             myRB.velocity = new Vector2(myRB.velocity.x, 0f);
         }
