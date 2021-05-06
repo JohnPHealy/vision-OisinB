47a9dc65ae0b1f180a742bedc940d9a86a23b660
oisin <c20387516@mytudublin.ie>
2021-05-06 16:41:50 +0100

    Movement completed

---
+++ Vision/Assets/Scripts/LoadZone.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadZone   :   MonoBehaviour
+{
+    private void OnCollisionEnter(Collision other)
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+}
